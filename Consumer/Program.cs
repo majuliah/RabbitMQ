148cba6a8a0b12a5b8b46948bc86e87b42414733
Majulia <majulialcarvalho@gmail.com>
2024-06-12 19:08:22 -0300

    testado

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -10,30 +10,30 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())//criando canal da conexão
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();//criando canal da conexão
+
+            channel.QueueDeclare(queue: "hello_1", //declarando a fila na qual vamos consumir a mensagem
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel); //solicita a entrega das mensagens de forma assincrona e fornece um retorno de chamada
+
+            consumer.Received += (model, ea) => //recebe a mensagem da fila e converte em string
             {
-                channel.QueueDeclare(queue: "hello_1", //declarando a fila na qual vamos consumir a mensagem
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel); //solicita a entrega das mensagens de forma assincrona e fornece um retorno de chamada
-
-                consumer.Received += (model, ea) => //recebe a mensagem da fila e converte em string
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    WriteLine($" Recebida: {message}");
-                };
-
-                channel.BasicConsume(queue: "hello_1", //indicando o consumo da mensagem
-                                    autoAck: true,
-                                    consumer: consumer);
-                ReadLine();
-            }
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                WriteLine($" Recebida: {message}");
+            };
+            channel.BasicConsume(queue: "hello_1", //indicando o consumo da mensagem
+                                autoAck: true,
+                                consumer: consumer);
+            ReadLine();
         }
     }
 }
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -12,31 +12,31 @@ namespace Publisher
         {
             //projeto que vai publicar a mensagem
             //abrindo uma conexão com o client .net
-            var factory = new ConnectionFactory()
-            {
-                HostName = "localhost"
-            };
+            var factory = new ConnectionFactory{ HostName = "localhost" };
 
-            using (var connection = factory.CreateConnection())//abrindo a conexão com o rabbit
-            
-            using (var channel = connection.CreateModel())
-            {
+            using var connection = factory.CreateConnection();//abrindo a conexão com o rabbit
+
+            using var channel = connection.CreateModel();
                 //para utilizar o rabbit, é necessário criar um nó rabbit.
                 //conexão esta que será utilizada para manipular todas as operações do mq
-                channel.QueueDeclare(queue: "hello_1", //nomeDaFila
-             /*declarando uma fila*/   durable: false, //permanece ativa se o servidor reiniciado
+            channel.QueueDeclare(queue: "hello_1", //nomeDaFila
+             /*declarando uma fila*/ durable: false, //permanece ativa se o servidor reiniciado
                                      exclusive: false, //acessada via conexão atual, excluída ao fechar conexão
                                      autoDelete: false,//será deletada automaticamente ao fim da fila
-                                     arguments: null); 
-                string message = "Primeiro teste com Rabbit 🐰"; //mensagem postada na fila
+                                     arguments: null);
+
+                const string message = "Primeiro teste com Rabbit 🐰"; //mensagem postada na fila
                 var body = Encoding.UTF8.GetBytes(message);      //codificando em array de bytes
 
-                channel.BasicPublish(exchange: "",  //publicando a fila
-                                     routingKey: "hello_1",
-                                     basicProperties: null,
-                                     body: body); //corpo da mensagem
+
+                channel.BasicPublish(exchange: string.Empty,  //publicando a fila
+                                         routingKey: "hello_1",
+                                         basicProperties: null,
+                                         body: body); //corpo da mensagem
+
                 WriteLine($" Enviada: {message}");
-            }
+            
+            ReadLine();
         }
     }
 }
