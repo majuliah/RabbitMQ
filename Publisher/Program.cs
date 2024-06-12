6224094364db5d366033eee2675b5890b84dfb86
Majulia <majulialcarvalho@gmail.com>
2024-06-12 10:11:53 -0300

    first commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
