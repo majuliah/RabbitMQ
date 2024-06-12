8002dbe66478191bba2e78733349813ca776650f
Majulia <majulialcarvalho@gmail.com>
2024-06-12 12:19:16 -0300

    concluindo tutorial hello world

---
+++ DocumentationGuide/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace DocumentationGuide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Receive();
+            Sender();
+        }
+
+        static void Receive()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            WriteLine(" [*] Waiting for messages.");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                WriteLine($" [x] Received {message}");
+            };
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            WriteLine(" Press [enter] to exit.");
+            ReadLine();
+        }
+        static void Sender()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            WriteLine($" [x] Sent {message}");
+
+            WriteLine(" Press [enter] to exit.");
+            ReadLine();
+        }
+    }
+}
